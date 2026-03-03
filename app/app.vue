80f1e761374a3b537f25c0313fb07306ee86c4de
raylo <raylotane@gmail.com>
2026-03-03 21:28:15 +0800

    feat: use element plus lib

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    this is app
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <LazyElButton type="warning">lazy button</LazyElButton>
   </div>
 </template>
